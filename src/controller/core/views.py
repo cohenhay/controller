47d6da3833ecd01543509f884b4b3be36f1fe08e
asaf93@gmail.com <asaf.yaron@Verint.com>
2020-07-20 10:06:32 +0300

    clean view

--- src/controller/core/views.py
+++ src/controller/core/views.py
@@ -11,34 +11,3 @@ def search(request):
     servers_list = Server.objects.all()
     server_filter = CoreFilter(request.GET, queryset=servers_list)
     return render(request, 'home.html', {'filter': server_filter})
-
-# class HomeView(ListView):
-   
-
-
-#     model = Server
-#     template_name = 'home.html'
-#     def get_context_data(self, **kwargs):
-#         context = super().get_context_data(**kwargs)
-#         context['filter'] = CoreFilter(self.request.GET, queryset=set.get_queryset())
-#         return render(request , template_name, context)
-
-    # def get(self,request, *args, **kwargs):
-    #     queryset = Server.objects.all()
-    #     context = {
-    #         "object_list":queryset
-    #     }
-    #     return render(request , "home.html" , context)
-    
-    
-    # def get_queryset(self,request, *args, **kwargs):
-    #     queryset = self.request.GET.get('q')
-    #     object_list = Server.objects.filter(Q(hostname__icontains=queryset) | Q(ipadress__icontains=queryset) | Q(os__icontains=queryset))
-    #     context = {
-    #         "object_list":queryset
-    #     }
-    #     return render(request , "home.html" , context)
-
-
-
-
